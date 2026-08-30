7e4e127d0277c74d1beab127e13498c3c98bcb52
Bogdan <bogdandeveloper@protonmail.com>
2026-08-30 21:37:25 +0100

    Added movement

--- main.lua
+++ main.lua
@@ -1,11 +1,22 @@
 function love.load()
     x = 5
+    y = 5
 end
 
 function love.update(dt)
-    x = x + 100 * dt
+    if love.keyboard.isDown("right") then
+        x = x + 200 * dt
+    elseif love.keyboard.isDown("left") then
+        x = x - 200 * dt
+    end
+    
+    if love.keyboard.isDown("up") then
+        y = y - 200 * dt
+    elseif love.keyboard.isDown("down") then
+        y = y + 200 * dt
+    end
 end
 
 function love.draw()
-    love.graphics.rectangle("line", x, 50, 200, 150)
+    love.graphics.rectangle("line", x, y, 200, 150)
 end
