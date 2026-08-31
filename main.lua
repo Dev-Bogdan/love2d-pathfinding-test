191f05be152d6f83b759e3fe15fece772b49e83c
Bogdan <bogdandeveloper@protonmail.com>
2026-08-31 11:14:55 +0100

    Refatctored  movement to be linked to mouse cursor, added rudimentary pathfinding

--- main.lua
+++ main.lua
@@ -1,22 +1,44 @@
 function love.load()
-    x = 5
-    y = 5
+    circle = {}
+
+    circle.x = 100
+    circle.y = 100
+    circle.radius = 25
+    circle.speed = 200
+end
+
+function getDistance(x1, y1, x2, y2)
+    local horizontal_distance = x1 - x2
+    local vertical_distance = y1 - y2
+    local a = horizontal_distance * horizontal_distance
+    local b = vertical_distance * vertical_distance
+
+    local c = a + b
+    local distance = math.sqrt(c)
+    return distance
 end
 
 function love.update(dt)
-    if love.keyboard.isDown("right") then
-        x = x + 200 * dt
-    elseif love.keyboard.isDown("left") then
-        x = x - 200 * dt
-    end
-    
-    if love.keyboard.isDown("up") then
-        y = y - 200 * dt
-    elseif love.keyboard.isDown("down") then
-        y = y + 200 * dt
+    mouse_x, mouse_y = love.mouse.getPosition()
+    angle = math.atan2(mouse_y - circle.y, mouse_x - circle.x)
+    cos = math.cos(angle)
+    sin = math.sin(angle)
+
+    local distance = getDistance(circle.x, circle.y, mouse_x, mouse_y)
+
+    -- enemy only moves if player actor is within 400 pixels
+    if distance < 400 then
+        circle.x = circle.x + circle.speed * cos * (distance/100) * dt
+        circle.y = circle.y + circle.speed * sin * (distance/100) * dt
     end
 end
 
 function love.draw()
-    love.graphics.rectangle("line", x, y, 200, 150)
+    love.graphics.circle("line", circle.x, circle.y, circle.radius)
+    love.graphics.line(circle.x, circle.y, mouse_x, mouse_y)
+    love.graphics.line(circle.x, circle.y, mouse_x, circle.y)
+    love.graphics.line(mouse_x, mouse_y, mouse_x, circle.y)
+
+    local distance = getDistance(circle.x, circle.y, mouse_x, mouse_y)
+    love.graphics.circle("line", circle.x, circle.y, distance)
 end
